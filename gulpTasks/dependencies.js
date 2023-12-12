0b6ae6dcd18c9e63f0ea7443b6301615db5b4282
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-12 18:33:04 -0300

    Task Dependencies

--- gulpTasks/dependencies.js
+++ gulpTasks/dependencies.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp');
+const uglifycss = require('gulp-uglifycss');
+const concat = require('gulp-concat');
 
-function dependenceCSS(callBack) {
-    return callBack();
+function dependenceCSS() {
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+    .pipe(uglifycss({"uglyComments" : false}))
+    .pipe(concat('dependencies.min.css'))
+    .pipe(gulp.dest('build/assets/css'));
 }
 
-function dependenceFonts(callBack) {
-    return callBack();
+function dependenceFonts() {
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+    .pipe(gulp.dest('build/assets/fonts'));
 }
 
 module.exports = {
