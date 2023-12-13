a876e68671775494086c4756ea7faf9aaebb4059
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-12-13 00:01:12 -0300

    Task Server

--- gulpTasks/server.js
+++ gulpTasks/server.js
@@ -1,10 +1,22 @@
 const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+const watch = require('gulp-watch');
 
-function monitorFiles(callBack) {
-    return callBack();
+function server() {
+    return gulp.src('build')
+    .pipe(webserver({
+        port: 8080,
+        open: true,
+        livereload: true,
+    }));
 }
 
-function server(callBack) {
+function monitorFiles(callBack) {
+    watch('src/**/*.html', () => gulp.series('appHTML')());
+    watch('src/**/*.scss', () => gulp.series('appCSS')());
+    watch('src/**/*.js', () => gulp.series('appJS')());
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')());
+    
     return callBack();
 }
 
